1da2c1f386ef4e17021797dd9205a4083dfaa7df
devinmoreland <devinmoreland20@gmail.com>
2022-07-07 16:04:47 -0500

    Clean Up with Folders

--- Buckets/delete-bucket.py
+++ Buckets/delete-bucket.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.9
 import boto3
 s3 = boto3.client('s3')
-BUCKET_NAME = <INSERT_BUCKET_NAME>
+BUCKET_NAME = "insertbucketname"
 response = s3.delete_bucket(
     Bucket = BUCKET_NAME
 )
